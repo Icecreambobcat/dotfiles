799348a549110a330a51376014f7a7e24f0082c9
Icecreambobcat <icekingrex123@gmail.com>
2025-02-03 00:22:02 +1100

    non broken vimtex config

--- nvim/lua/configs/vimtex.lua
+++ nvim/lua/configs/vimtex.lua
@@ -21,3 +21,18 @@ config.vimtex_syntax_conceal = {
   sections = 0,
   styles = 1,
 }
+
+config.vimtex_compiler_latexmk = {
+  aux_dir = ".tmp",
+  out_dir = "",
+  callback = 1,
+  continuous = 1,
+  executable = "latexmk",
+  hooks = {},
+  options = {
+    "-verbose",
+    "-file-line-error",
+    "-synctex=1",
+    "-interaction=nonstopmode",
+  },
+}
