6a5e2be850e98bee076eaa7f9ee99ae39270c1c2
Icecreambobcat <icekingrex123@gmail.com>
2025-04-02 08:40:11 +1100

    temp fix for broken cheatsheet

--- nvim/lua/chadrc.lua
+++ nvim/lua/chadrc.lua
@@ -101,7 +101,7 @@ local options = {
 
   cheatsheet = {
     theme = "grid", -- simple/grid
-    excluded_groups = { "terminal (t)", "autopairs", "Nvim", "Opens" }, -- can add group name or with mode
+    -- excluded_groups = { "terminal (t)", "autopairs", "Nvim", "Opens" }, -- can add group name or with mode
   },
 
   mason = { pkgs = {} },
