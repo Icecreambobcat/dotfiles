2345ccc43ffed26115a5fff44849f1f83635ff4a
Icecreambobcat <icekingrex123@gmail.com>
2025-05-25 20:45:54 +1000

    fix spec

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -314,53 +314,51 @@ return {
       "rcarriga/nvim-notify",
     },
     event = "VeryLazy",
-    config = function()
-      require("noice").setup {
-        routes = {
-          {
-            view = "notify",
-            filter = {
-              event = "msg_showmode",
-              any = {
-                { find = "recording" },
-              },
+    opts = {
+      routes = {
+        {
+          view = "notify",
+          filter = {
+            event = "msg_showmode",
+            any = {
+              { find = "recording" },
             },
           },
         },
+      },
 
-        lsp = {
-          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-          override = {
-            -- override the default lsp markdown formatter with Noice
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            -- override the lsp markdown formatter with Noice
-            ["vim.lsp.util.stylize_markdown"] = true,
-            -- override cmp documentation with Noice (needs the other options to work)
-            ["cmp.entry.get_documentation"] = true,
-          },
-          hover = { enabled = true }, -- <-- HERE!
-          signature = {
+      lsp = {
+        -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+        override = {
+          -- override the default lsp markdown formatter with Noice
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          -- override the lsp markdown formatter with Noice
+          ["vim.lsp.util.stylize_markdown"] = true,
+          -- override cmp documentation with Noice (needs the other options to work)
+          ["cmp.entry.get_documentation"] = true,
+        },
+        hover = { enabled = true }, -- <-- HERE!
+        signature = {
+          enabled = true,
+          auto_open = {
             enabled = true,
-            auto_open = {
-              enabled = true,
-              trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
-              luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
-              throttle = 50, -- Debounce lsp signature help request by 50ms
-            },
-            view = nil, -- when nil, use defaults from documentation
-            opts = {}, -- merged with defaults from documentation
+            trigger = true, -- Automatically show signature help when typing a trigger character from the LSP
+            luasnip = true, -- Will open signature help when jumping to Luasnip insert nodes
+            throttle = 50, -- Debounce lsp signature help request by 50ms
           },
+          view = nil, -- when nil, use defaults from documentation
+          opts = {}, -- merged with defaults from documentation
         },
-        -- you can enable a preset for easier configuration
-        presets = {
-          bottom_search = false, -- use a classic bottom cmdline for search
-          command_palette = false, -- position the cmdline and popupmenu together
-          long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = false, -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = true, -- add a border to hover docs and signature help
-        },
-      }
-    end,
+      },
+      -- you can enable a preset for easier configuration
+      presets = {
+        bottom_search = false, -- use a classic bottom cmdline for search
+        command_palette = false, -- position the cmdline and popupmenu together
+        long_message_to_split = true, -- long messages will be sent to a split
+        inc_rename = false, -- enables an input dialog for inc-rename.nvim
+        lsp_doc_border = true, -- add a border to hover docs and signature help
+      },
+    },
   },
   {
     "stevearc/dressing.nvim",
