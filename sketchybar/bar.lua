9fb3595f0f05fb3d60f6fbb4116b56a6b87f20cf
Icecreambobcat <icekingrex123@gmail.com>
2025-05-28 13:16:03 +1000

    update sketchybar spec

--- sketchybar/bar.lua
+++ sketchybar/bar.lua
@@ -2,8 +2,11 @@ local colors = require("colors")
 
 -- Equivalent to the --bar domain
 sbar.bar({
-  height = 40,
-  color = colors.bar.bg,
-  padding_right = 2,
-  padding_left = 2,
+	height = 40,
+	color = colors.bar.bg,
+	padding_right = 0,
+	padding_left = 0,
+	corner_radius = 12,
+	y_offset = 6,
+	margin = 4,
 })
--- sketchybar/colors.lua
+++ sketchybar/colors.lua
@@ -1,28 +1,31 @@
 return {
-  black = 0xff181819,
-  white = 0xffe2e2e3,
-  red = 0xfffc5d7c,
-  green = 0xff9ed072,
-  blue = 0xff76cce0,
-  yellow = 0xffe7c664,
-  orange = 0xfff39660,
-  magenta = 0xffb39df3,
-  grey = 0xff7f8490,
-  transparent = 0x00000000,
+	black = 0xff414868,
+	white = 0xffc0caf5,
+	red = 0xfff7768e,
+	green = 0xff9ece6a,
+  cyan = 0xff2ac3de,
+	blue = 0xff7aa2f7,
+	yellow = 0xffe0af68,
+	orange = 0xffff9e64,
+	magenta = 0xffbb9af7,
+	grey = 0xff565f89,
+	transparent = 0x00000000,
 
-  bar = {
-    bg = 0xb42c2e34,
-    border = 0xff2c2e34,
-  },
-  popup = {
-    bg = 0xc02c2e34,
-    border = 0xff7f8490
-  },
-  bg1 = 0xff363944,
-  bg2 = 0xff414550,
+	bar = {
+		bg = 0xc41a1b26,
+		border = 0xffc0caf5,
+	},
+	popup = {
+		bg = 0xc024283b,
+		border = 0xffc0caf5,
+	},
+	bg1 = 0xff1a1b26,
+	bg2 = 0xff24283b,
 
-  with_alpha = function(color, alpha)
-    if alpha > 1.0 or alpha < 0.0 then return color end
-    return (color & 0x00ffffff) | (math.floor(alpha * 255.0) << 24)
-  end,
+	with_alpha = function(color, alpha)
+		if alpha > 1.0 or alpha < 0.0 then
+			return color
+		end
+		return (color & 0x00ffffff) | (math.floor(alpha * 255.0) << 24)
+	end,
 }
