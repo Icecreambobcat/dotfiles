435b6474afa31a9803af6744f3ae0b65eaa19ac2
Icecreambobcat <icekingrex123@gmail.com>
2025-06-25 09:13:24 +1000

    CATPPUCCIN SHILL

--- sketchybar/colors.lua
+++ sketchybar/colors.lua
@@ -1,26 +1,26 @@
 return {
-	black = 0xff414868,
-	white = 0xffc0caf5,
-	red = 0xfff7768e,
-	green = 0xff9ece6a,
-  cyan = 0xff2ac3de,
-	blue = 0xff7aa2f7,
-	yellow = 0xffe0af68,
-	orange = 0xffff9e64,
-	magenta = 0xffbb9af7,
-	grey = 0xff565f89,
+	black = 0xff181825,
+	white = 0xffcdd6f4,
+	red = 0xfff38ba8,
+	green = 0xffa6e3a1,
+  cyan = 0xff94e2d5,
+	blue = 0xff89b4fa,
+	yellow = 0xfff9e2af,
+	orange = 0xfffab387,
+	magenta = 0xfff5c2e7,
+	grey = 0xff7f849c,
 	transparent = 0x00000000,
 
 	bar = {
-		bg = 0xc41a1b26,
-		border = 0xffc0caf5,
+		bg = 0xc4181825,
+		border = 0xff6c7086,
 	},
 	popup = {
-		bg = 0xc41a1b26,
-		border = 0xffc0caf5,
+		bg = 0xc4181825,
+		border = 0xff6c7086,
 	},
-	bg1 = 0xff1a1b26,
-	bg2 = 0xff24283b,
+	bg1 = 0xff1e1e2e,
+	bg2 = 0xff313244,
 
 	with_alpha = function(color, alpha)
 		if alpha > 1.0 or alpha < 0.0 then
