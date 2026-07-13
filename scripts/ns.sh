24c76b4583ec7bdbd693ab8a876cfa93c8498052
Icecreambobcat <icekingrex123@gmail.com>
2026-07-14 09:26:47 +1000

    raycast shortcuts bug workaroudn

---
+++ scripts/ns.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title NS
+# @raycast.mode silent
+
+# Optional parameters:
+# @raycast.icon 🌙
+
+# Documentation:
+# @raycast.author Icecreambobcat
+# @raycast.authorURL https://github.com/Icecreambobcat
+
+set -euo pipefail
+shortcuts run "NS"
